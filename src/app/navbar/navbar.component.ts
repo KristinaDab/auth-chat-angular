0d60fbee1668c9782add3c006fd73f86d880ebfe
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-02-27 15:54:53 +0100

    Add navbar.component.css, html and ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user: Observable<firebase.User>;
+  userEmail: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.user = this.authService.authUser();
+    this.user.subscribe(user => {
+      if (user) {
+        this.userEmail = user.email;
+      }
+    });
   }
 
+  logout() {
+    this.authService.logout();
+  }
+
+
+  login() {
+    this.authService.login("email", "password");
+  }
 }
+
+
