ccc7b0f6d41460b50d245605310ef6c5a246ee40
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-02-27 15:52:17 +0100

    Add scrollToBottom() functionality chatroom.component.ts
    So the newest messages will be always in view instead of the top oldest messages

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-chatroom',
   templateUrl: './chatroom.component.html',
   styleUrls: ['./chatroom.component.css']
 })
-export class ChatroomComponent implements OnInit {
+export class ChatroomComponent implements OnInit, AfterViewChecked {
+
+  // ElementRef will give access to underlying DOM element
+  // @ViewChild will give access to the template reference we have
+  // in the template
+  @ViewChild('scroller') private feedContainer: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // This method will be called after ngAfterViewChecked() method
+  // and will scroll to the bottom of messages, so we can see the newest
+
+  scrollToBottom() : void {
+    // Element.scrollTop gets or sets the number of pixels that
+    // an element's content is scrolled vertically.
+
+    // Element.scrollHeight read-only property to get the height
+    // of an element's content, including overflow
+    this.feedContainer.nativeElement.scrollTop 
+    = this.feedContainer.nativeElement.scrollHeight;
+  }
+
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class. (Check Lifecycle Hooks)
+    this.scrollToBottom();
+  }
 }
