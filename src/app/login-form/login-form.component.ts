f64a7b09ed957c5c1cc6d0f9318a67ad5afd4253
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-02-27 15:53:28 +0100

    Add login-form functionality, html and style

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  email: string;
+  password: string;
+  errorMsg: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    console.log('login() called from login-form component');
+    this.authService.login(this.email, this.password)
+    .catch(error => this.errorMsg = error.message);
+  }
 }
