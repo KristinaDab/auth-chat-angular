47a2b63ef05b0e3d3d1168e4e41b503c5a14b1a8
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-02-27 18:37:02 +0100

    Add AngularFireAuthGuard to app-routing.module.ts
    To take care of pages accessibility based on logged in status

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo} from '@angular/fire/auth-guard';
 
+// Using AngularFireAuthGuard to allow or not allow users to access specific pages based
+// on if they are logged in or not 
+
+const redirectUnauthorizedToLogin= () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToChat = () => redirectLoggedInTo(['chat']);
 
 const routes: Routes = [
-  {path: 'signup', component: SignupFormComponent},
-  {path: 'login', component: LoginFormComponent},
-  {path: 'chat', component: ChatroomComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'signup', component: SignupFormComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedInToChat }},
+  {path: 'login', component: LoginFormComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedInToChat }},
+  {path: 'chat', component: ChatroomComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin}}
 ];
 
 @NgModule({
