edd5ecb33b46dbf2254cf6abf887e40e6fa82c94
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-02-28 19:00:52 +0100

    Add contacts.service.ts

---
+++ src/app/services/contacts.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import 'firebase/database';
+import { Contact } from './../models/contact.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ContactsService {
+
+  contactsRef : AngularFireList<any>; // Reference to Contact data list, its an Observable
+  contactRef: AngularFireObject<any>;  // Reference to Contact object, its an Observable too
+
+  // Inject AngularFireDatabase Dependency in Constructor
+  constructor(private db: AngularFireDatabase) { }
+
+  // Create Contact
+  AddContact(contact: Contact) {
+    this.contactsRef.push({
+      name: contact.name,
+      address: contact.address,
+      email: contact.email,
+      avatar: contact.avatar
+    })
+  }
+
+  // Fetch Single Contact Object
+  GetContact(id: string) {
+    this.contactRef = this.db.object('contacts-list/' + id);
+    return this.contactRef;
+  }
+
+  // Fetch Contact List
+  GetContactsList() {
+    this.contactsRef = this.db.list('contacts-list');
+    return this.contactsRef;
+  }  
+
+  // Update Contact Object
+  UpdateContact(contact: Contact) {
+    this.contactRef.update({
+      name: contact.name,
+      address: contact.address,
+      email: contact.email,
+      avatar: contact.avatar
+    })
+  }  
+
+  // Delete Student Object
+  DeleteContact(id: string) { 
+    this.contactRef = this.db.object('contacts-list/'+id);
+    this.contactRef.remove();
+  }
+}
