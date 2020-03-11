dda128cfea0b4079d91d6ce31a30571e63d4bd1f
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-03-11 15:18:39 +0100

    Add comments to auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,9 +14,9 @@ import 'firebase/auth';
 })
 
 export class AuthService {
+
   private user: Observable<firebase.User>;
   private authState: any;
-  // public loggedIn = false;
   userData: firebase.User;
 
   constructor(
@@ -30,11 +30,14 @@ export class AuthService {
       return this.user;
     }
 
+    // Get the uid of the current signed-in user
     get currentUserId(): string {
       var currentUser = firebase.auth().currentUser;
       return this.authState !== null ? currentUser.uid : '';
     }
 
+    // Log in with email and password, updating user auth state and
+    // setting user status to 'online'
     login(email: string, password: string) {
       return firebase.auth().signInWithEmailAndPassword(email, password)
         .then((user) => {
@@ -44,6 +47,7 @@ export class AuthService {
         });
     }
 
+    // Log out setting user status 'offline'
     logout() {
       this.setUserStatus('offline');
       return firebase.auth().signOut().then(() => {
@@ -52,7 +56,9 @@ export class AuthService {
       
       
     }
-
+    // Sign-up wiht email, password and displayName, update user authState
+    //  with a new logged-in user, set status 'online' and setUserData to 
+    //  save a new user to the database 
     signUp(email: string, password: string, displayName: string) {
       return firebase.auth().createUserWithEmailAndPassword(email, password)
               .then((user) => {
@@ -62,6 +68,7 @@ export class AuthService {
               }).catch(error => console.log(error));
     }
 
+    // Saves newly signed up user object to the database with current user id 
     setUserData(email: string, displayName: string, status: string): void {
       const path = `users/${this.currentUserId}`;
       const data = {
@@ -69,19 +76,16 @@ export class AuthService {
         displayName: displayName,
         status: status
       };
-
       this.db.object(path).update(data)
         .catch(error => console.log(error));
     }
 
-
+    // Set user status 'online' or 'offline' 
     setUserStatus(status: string): void {
       const path = `users/${this.currentUserId}`;
-
       const data = {
         status: status
       };
-
       this.db.object(path).update(data)
         .catch(error => console.log(error));
     }
