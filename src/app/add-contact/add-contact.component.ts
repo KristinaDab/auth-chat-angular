6681743c025bc4f015af7a45565f9d523c5f8ae4
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-03-12 13:49:05 +0100

    Add comments and tidy up the code- add-contact.component.ts and html

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ContactsService } from './../services/contacts.service'; // CRUD services API
+import { ContactsService } from './../services/contacts.service';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'; // Reactive form services
 import { ToastrService } from 'ngx-toastr'; // Alert message using NGX toastr
-import { Location } from '@angular/common';  // Location service is used to go back to previous component
+import { Location } from '@angular/common';  
 import { AuthService } from './../services/auth.service';
 
 
@@ -16,7 +16,7 @@ export class AddContactComponent implements OnInit {
   public contForm: FormGroup;  // Define FormGroup to contact's form
  
   constructor(
-    public dbApi: ContactsService,  // CRUD API services
+    public contactDB: ContactsService,  // CRUD contact service
     public fb: FormBuilder,       // Form Builder service for Reactive forms
     private location: Location,         // Location service to go back to previous component
     public toastr: ToastrService,  // Toastr service for alert message
@@ -25,14 +25,14 @@ export class AddContactComponent implements OnInit {
 
  
   ngOnInit() {
-    this.dbApi.getContactsList();  // Call GetSContactsList() before main form is being called
-    this.contactForm();              // Call contact form when component is ready
+    this.contactDB.getContactsList();  // Call GetContactsList() before main form is being called
+    this.contactForm();              // Call contact form method when component is ready
   }
 
   // Reactive contact form
   contactForm() {
     this.contForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
+      name: [''],
       address: [''],
       email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
       avatar: [''],
@@ -66,15 +66,15 @@ export class AddContactComponent implements OnInit {
     this.contForm.reset();
   }  
 
+  // Submit contact data to the database
   submitContactData() {
-    this.dbApi.addContact(this.contForm.value); // Submit contact data using db API
+    this.contactDB.addContact(this.contForm.value); // Submit contact data using ContactsService
     this.toastr.success(this.contForm.controls['name'].value + ' successfully added!'); // Show success message when data is successfully submited
     this.ResetForm();  // Reset form when clicked on reset button
     console.log(this.auth.currentUserId);
-    
    };
 
-     // Go back to previous component
+  // Go back to previous component
   goBack() {
     this.location.back();
   }
