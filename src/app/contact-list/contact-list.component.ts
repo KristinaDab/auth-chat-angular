85b753b74003ddd48e631ca1c14be92367c4fe6c
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-03-12 11:50:54 +0100

    Add comments and clean up code in contact-list.html and ts

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -10,23 +10,23 @@ import { ToastrService } from "ngx-toastr"; // Alert message using NGX toastr
   styleUrls: ["./contact-list.component.css"]
 })
 export class ContactListComponent implements OnInit {
-  p: number = 1; // Settup up pagination variable
   Contact: Contact[]; // Save contacts data in Contact's array.
-  hideWhenNoContact: boolean = false; // Hide contacts data table when no student.
-  noData: boolean = false; // Showing No Contact Message, when no contact in database.
-  preLoader: boolean = true; // Showing Preloader to show user data is coming for you from thre server(A tiny UX Shit)
+  hideWhenNoContact: boolean = false; // Hide contacts data table when no contact.
+  noData: boolean = false; // Showing 'No Contact' Message, when no contact in database.
+  preLoader: boolean = true; // Showing Preloader to show user data is coming for you from the server
 
   constructor(
-    public dbApi: ContactsService, // Inject contact CRUD services in constructor.
+    public contactDB: ContactsService, // Inject contact CRUD services in constructor.
     public toastr: ToastrService, // Toastr service for alert message
     public auth: AuthService
   ) {}
 
   ngOnInit() {
     this.dataState(); // Initialize contact's list, when component is ready
-    let s = this.dbApi.getContactsList();
+    let s = this.contactDB.getContactsList();
     s.snapshotChanges().subscribe(data => {
-      // Using snapshotChanges() method to retrieve list of data along with metadata($key)
+      // Using snapshotChanges() method to retrieve
+      //  list of data along with metadata($key)
       this.Contact = [];
       data.forEach(item => {
         let a = item.payload.toJSON();
@@ -36,9 +36,11 @@ export class ContactListComponent implements OnInit {
     });
   }
 
-  // Using valueChanges() method to fetch simple list of contacts data. It updates the state of hideWhenNoContact, noData & preLoader variables when any changes occurs in contact data list in real-time.
+  // Using valueChanges() method to fetch simple list of contacts data.
+  // It updates the state of hideWhenNoContact, noData & preLoader variables
+  //  when any changes occurs in contact data list in real-time.
   dataState() {
-    this.dbApi
+    this.contactDB
       .getContactsList()
       .valueChanges()
       .subscribe(data => {
@@ -55,9 +57,9 @@ export class ContactListComponent implements OnInit {
 
   // Method to delete contact object
   deleteContact(contact) {
+    // Asking from user before Deleting contact data.
     if (window.confirm("Are sure you want to delete this contact ?")) {
-      // Asking from user before Deleting contact data.
-      this.dbApi.deleteContact(contact.$key); // Using Delete student API to delete contact.
+      this.contactDB.deleteContact(contact.$key); // Using Delete contact method from ContactsService to delete contact.
       this.toastr.success(contact.name + " successfully deleted!"); // Alert message will show up when contact successfully deleted.
     }
   }
